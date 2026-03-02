0bb76b385c9b9044bbb775b054ce23812a8e69a9
Franco Cattani <profefrancocattani@gmail.com>
2026-03-02 14:53:56 -0300

    Configuración de despliegue automático

--- calendario-san-jose/vite.config.js
+++ calendario-san-jose/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/calendario-san-jose/', // Esto es vital para que cargue en GitHub
 })
